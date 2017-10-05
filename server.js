17e5243c64956528bf213e21bbe49fac1fa6ac00
root <israel_batista.am@hotmail.com>
2017-10-05 13:02:51 -0400

    change files

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var session=require('express-session');
 var Twitter = require("node-twitter-api");
 
     var twitter = new Twitter({
-        consumerKey: "uoqmgicgFUARcjKeds78ocVlj",
-      consumerSecret:"rGDB6sf7qk9hoiDxszEPPGvWlg5V78vEvzethOy6QNPt3DS1AU",
-      callback: "https://nightlifeapp-isrmm.herokuapp.com/access-token"
+        consumerKey: "l0Q11C3VqtdLze9jQCHhQiX4V",
+      consumerSecret:"sdPtwLeAdlr3cR9WuPki6gCagFtBCuqx97fZn62dSktMf3yI7Z",
+      callback: "https://pinterest-clone-imm.herokuapp.com/access-token"
     });
-  
+
 
 
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
@@ -51,3 +51,35 @@ app.listen(port, function () {
 app.post('/signin', function(req, res) {
 	
 });
+
+
+app.get("/request-token", function(req, res) {
+        twitter.getRequestToken(function(err, requestToken, requestSecret) {
+            if (err)
+                res.status(500).send(err);
+            else {
+                _requestSecret = requestSecret;         
+                res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+            }
+        });
+    });
+  
+  
+ app.get("/access-token", function(req, res) {
+        var requestToken = req.query.oauth_token,
+      verifier = req.query.oauth_verifier;
+
+        twitter.getAccessToken(requestToken, _requestSecret, verifier, function(err, accessToken, accessSecret) {
+            if (err)
+                res.status(500).send(err);
+            else
+                twitter.verifyCredentials(accessToken, accessSecret, function(err, user) {
+                    if (err)
+                        res.status(500).send(err);
+                    else{
+                        req.session.user = user; 
+                        res.redirect("https://pinterest-clone-imm.herokuapp.com/");
+                    }
+                });
+        });
+    });
