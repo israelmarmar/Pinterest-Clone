ab3b17a0f9849198a587d4088ec5094b69d50484
israel marques martins <emaill.israel.br@gmail.com>
2017-10-15 15:25:22 -0400

    change files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var changebook=false;
+var changepins=false;
 
 function serialize(form) {
     var result = [];
@@ -44,6 +44,54 @@ if(document.cookie && getCookie("user")!=="undefined"){
   console.log(username);
   }
 
+ var Blike= React.createClass({
+	 
+	 getInitialState: function () {
+    return { 
+            likes:this.props.likes
+
+           };
+  },
+	 over: function(e){
+    var div=lik=e.target.parentElement.childNodes;
+    var lik=div[1];
+    var usr=div[0];
+    lik.classList.remove("hid");
+    usr.classList.remove("hid");
+	},
+
+	like: function(e){
+		e.target.classList.add("disabled");
+    var th = this;
+    this.serverRequest = 
+      axios.get("/like?id="+e.target.id)
+     
+        .then(function(result) {    
+	
+	
+	if(result){
+		console.log("c");
+    e.target.classList.remove("disabled");
+	
+          th.setState({
+            likes: result.data.likes,
+          });
+		  
+		  if(parseInt(result.data.likes)>e.target.value)
+		  e.target.setAttribute("style", "color: "+"#337ab7");
+		  else
+	      e.target.setAttribute("style", "color: "+"black");
+	}
+      
+        })
+		},
+  
+	 render: function () {
+		return (
+          <button onMouseOver={this.over} style={this.props.style} id={this.props.id} onClick={this.like} className={this.props.className}>{this.state.likes}</button>)
+         }
+ });
+  
 var Photos= React.createClass({
 
   getInitialState: function () {
@@ -87,30 +135,18 @@ var Photos= React.createClass({
     usr.classList.add("hid");
   },
 
-  like: function(e){
-    var th = this;
-    this.serverRequest = 
-      axios.get("/like?id="+e.target.id)
-     
-        .then(function(result) {    
-      
-          e.target.value=result.data.likes;
-      
-        })
-  },
-
   render: function () {
     var th=this;
     return (<div className='grid'>
       {this.state.data.map(function(item) {
         var w=parseInt(item.size.split("x")[0]);
         var h=parseInt(item.size.split("x")[1]);
-
+		var mypin=item.likes.map(x => x.user).indexOf(user.screen_name)!==-1;
           return (
-          <div className="div-item grid-item" style={{"height": h/4, "width": w/4}}>
-          <button onMouseOver={th.over} id="user" className={"user hid btn btn-default btn-sm glyphicon glyphicon glyphicon-user"}/>
-          <button onMouseOver={th.over} id={item._id} onClick={th.like} className={"lk hid btn btn-default btn-sm glyphicon glyphicon-star "+(user?"":"disabled")}>{item.likes.length}</button>
-          <img onMouseOver={th.over} onMouseOut={th.out} className="grid-item" src={item.img} style={{"height": h/4, "width": w/4}}/>
+          <div className="grid-item" style={{"height": h/4, "width": w/4}}>
+          <button onMouseOver={th.over} id="user" title={item.user} className={"user hid btn btn-default btn-sm glyphicon glyphicon glyphicon-user"}/>
+        <Blike id={item._id} likes={item.likes.length} style={{"color": mypin?"#337ab7":"black"}} className={"lk hid btn btn-default btn-sm glyphicon glyphicon-star "+(user?"":"disabled")}/>
+		 <img onMouseOver={th.over} onMouseOut={th.out} className="imgpin grid-item" src={item.img} style={{"height": h/4, "width": w/4}}/>
           </div>)
       })}
       </div>)
@@ -119,17 +155,45 @@ var Photos= React.createClass({
 });
 
 var Container = React.createClass({
+	
+	 submitform: function(ev){
+
+    ev.preventDefault();
+	var btn=ev.target.submit;
+	btn.classList.add("disabled");
+	var value=btn.innerHTML;
+    btn.innerHTML="<i class='fa fa-circle-o-notch fa-spin'></i>"+btn.innerHTML;
+	
+	$.ajax({
+            type: ev.target.method,
+            url: ev.target.action,
+            data: serialize(ev.target),
+            success: function (data) {
+			
+			if(data.msg=="ok"){
+			btn.innerHTML=value;
+            btn.classList.remove('disabled');
+			}
+			
+			}
+	});
+	
+	  },
 
   submit: function(){
 
     window.location.href="/request-token";
   },
-
+	
+	drop: function(e){
+	document.getElementById('drop').classList.add("showdrop");  
+	},
 
   render: function () {
-
+	var th=this;
   return(<div className="cont">
              <div className="navbar">
+			 
              <img className="icoheader" src="picasa_21755.png" width="50" height="50" />
              {() => {
                 
@@ -148,7 +212,7 @@ var Container = React.createClass({
 
                 if(user)
                   return(
-                    <div className="btnnav">All</div>
+                    <div id="all" className="btnnav">All</div>
                     )
                 }()}
 
@@ -156,14 +220,14 @@ var Container = React.createClass({
 
                 if(user)
                   return(
-                    <div className="btnnav">My Pics</div>
+                    <div id="mypics" className="btnnav">My Pics</div>
                     )
                 }()}
 
                 {() => {
                 if(user)
                   return(
-                    <div className="btnnav">Add Pic▾</div>
+                    <div id="addpic" onClick={th.drop} className="btnnav">Add Pic▾</div>
                     )
                 }()}
 
@@ -172,6 +236,15 @@ var Container = React.createClass({
 
              </div>
              <div className="page">
+			 
+			 <div id="drop" className="dropdown-menu">
+			 <form className="add-form" action="/addpin" method="post" onSubmit={this.submitform}>
+			 <input type="text" name="url" placeholder="Pic url..." className="form-control" />
+			 <input type="text" name="desc" placeholder="Pic description..." className="form-control" />
+			 <button id="submit" type="submit" className="btn btn-primary btn-block">Send</button>
+			 </form>
+			 </div>
+			 
              <Photos />
              </div>
         </div>)
@@ -182,22 +255,8 @@ var Container = React.createClass({
 
 ReactDOM.render(<Container />, document.getElementById('cont'));
 
+function hiddrop(){
+document.getElementById('drop').classList.remove("showdrop"); 	
+}
 
-
-
-$(function() {
-console.log("oi");
-
-
-$('.div-item').mouseover(function() {
-console.log("m");
-$('.div-item').children().css( "display", "block" );
-
-});
-
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 160
-});
-
-});
+document.body.addEventListener('click', hiddrop, true); 
